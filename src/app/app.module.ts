5d379d9819e49ed2ff0e713fbf0e3d59059dcf0d
pravink <pravinkawle@gmail.com>
2017-08-06 20:07:42 +0530

    Added reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
@@ -10,17 +10,20 @@ import { EmployeeService } from '../app/service/employee/employee.service';
 
 import { apiEndPoint, EndPoint } from '../app/service/api/api.value';
 import { ProductComponent } from './product/product.component';
+import { OrderComponent } from './order/order.component';
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
     UserComponent,
     UserdetailComponent,
-    ProductComponent
+    ProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [{ provide: EmployeeService, useClass: EmployeeService },
   { provide: EndPoint, useValue: apiEndPoint }],
