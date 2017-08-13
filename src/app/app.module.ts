b1b7d357a91685cb10ebedfe845beec5ec7415f4
pravink <pravinkawle@gmail.com>
2017-08-13 14:46:45 +0530

    Added Routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
@@ -10,7 +11,10 @@ import { EmployeeService } from '../app/service/employee/employee.service';
 
 import { apiEndPoint, EndPoint } from '../app/service/api/api.value';
 import { ProductComponent } from './product/product.component';
+import { CustomerComponent } from './customer/customer.component';
 import { OrderComponent } from './order/order.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,12 +22,22 @@ import { OrderComponent } from './order/order.component';
     UserComponent,
     UserdetailComponent,
     ProductComponent,
-    OrderComponent
+    OrderComponent,
+    CustomerComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule, RouterModule.forRoot([
+      { path: 'user', component: UserComponent },
+      { path: 'customer', component: CustomerComponent },
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'product', component: ProductComponent },
+      { path: 'order', component: OrderComponent },
+      { path: '', redirectTo: 'user', pathMatch: 'full' },
+      { path: '**', component: PagenotfoundComponent }
+    ])
   ],
   providers: [{ provide: EmployeeService, useClass: EmployeeService },
   { provide: EndPoint, useValue: apiEndPoint }],
